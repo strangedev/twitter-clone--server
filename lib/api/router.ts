020bf922d8ea83df346cb5573254b3486a7963e9
Noah Hummel <noah.hummel@thenativeweb.io>
2022-01-14 14:29:54 +0100

    fix: allow cors

--- lib/api/router.ts
+++ lib/api/router.ts
@@ -1,5 +1,6 @@
 import { authentication } from './middleware/authentication';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { getAccount } from './routes/accounts/getAccount';
 import { getAccountsTweets } from './routes/tweets/getAccountsTweets';
 import { getEveryonesTweets } from './routes/tweets/getEveryonesTweets';
@@ -11,6 +12,7 @@ import express, { Express } from 'express';
 const router = function (): Express {
   const app = express();
 
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(authentication());
 
